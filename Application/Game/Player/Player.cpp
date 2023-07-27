8e4f6319936f06a42f5c1bd51f47a4490091fa3e
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-07-27 16:17:20 +0900

    カメラからの距離に、カメラの注視点距離も渡す。

--- Application/Game/Player/Player.cpp
+++ Application/Game/Player/Player.cpp
@@ -188,9 +188,9 @@ void Player::OnCollision(const CollisionInfo& info) {
 }
 
 void Player::Reticle() {
-	Corsor cursor;
+	Corsor cursor{};
 
-	cursor.SetDistance(70);
+	cursor.SetDistance(70.0f + camera_->GetTarget().z);
 
 	//マウスカーソルから、3D照準座標を取得する
 	worldTransform3dReticle_.position_ =
