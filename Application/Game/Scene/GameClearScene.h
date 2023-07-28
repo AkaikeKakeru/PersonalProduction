9c59fce708bb7506b57490c54989239a83067f03
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-07-28 13:54:43 +0900

    ゲームクリアシーンとゲームオーバーシーンのクラスを宣言

---
+++ Application/Game/Scene/GameClearScene.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include "BaseScene.h"
+
+#include "Input.h"
+//#include "DrawBasis.h"
+//#include "Model.h"
+//#include "Sprite.h"
+//#include "Object3d.h"
+
+#include "Camera.h"
+#include "LightGroup.h"
+
+#include "SceneManager.h"
+#include "ImGuiManager.h"
+
+class GameClearScene : public BaseScene{
+public://構造体
+
+public:
+	void Initialize() override;
+	void Update() override;
+	void Draw() override;
+	void Finalize() override;
+private:
+	static DirectXBasis* dxBas_;
+	static Input* input_;
+	//DrawBasis* drawBas_ = nullptr;
+
+	Camera* camera_ = nullptr;
+	LightGroup* light_ = nullptr;
+
+	//ImGuiマネージャー
+	ImGuiManager* imGuiManager_ = nullptr;
+
+	/// <summary>
+	/// オブジェクト
+	/// </summary>
+	/// <summary>
+	//Object3d* planeObj_ = nullptr;
+	//Model* planeModel_ = nullptr;
+
+	//Object3d* skydomeObj_ = nullptr;
+	//Model* skydomeModel_ = nullptr;
+	///// スプライト
+	///// </summary>
+	//Sprite* sprite_ = new Sprite();
+};
