fbeb3314d14e2546e787d73b4fcc50e4a56b22b6
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-10-01 02:38:18 +0900

    削除したと思っていた関数が復活してしまったので削除しなおし

--- Application/Function/Gauge.cpp
+++ Application/Function/Gauge.cpp
@@ -190,38 +190,3 @@ void Gauge::ResetArrangement() {
 		positionGauge_.y
 	});
 }
-
-void Gauge::ResetSize() {
-	spriteRest_->SetSize({ size_.x * 5,size_.y });
-	spriteGauge_->SetSize({ size_.x * 4,size_.y });
-	spriteLeft_->SetSize({ size_.x,size_.y});
-	spriteRight_->SetSize({ size_.x,size_.y});
-}
-
-void Gauge::ResetPosition() {
-	spriteRest_->SetPosition({
-		positionGauge_.x + (lengthGauge_ * (lengthGauge_ / 2)) +
-		positionGaugeOffset_.x,
-		positionGauge_.y +
-		positionGaugeOffset_.y });
-
-	spriteGauge_->SetPosition({
-		positionGauge_.x + (lengthGauge_ * (lengthGauge_ / 2)) +
-		positionGaugeOffset_.x,
-		positionGauge_.y +
-		positionGaugeOffset_.y });
-
-	spriteLeft_->
-		SetPosition({
-		positionGauge_.x +
-		positionGaugeOffset_.x,
-			positionGauge_.y +
-			positionGaugeOffset_.y });
-
-	spriteRight_->
-		SetPosition({
-		positionGauge_.x + (lengthGauge_ * ((lengthGauge_ / 4) * (lengthGauge_ / 4))) +
-		positionGaugeOffset_.x,
-			positionGauge_.y +
-			positionGaugeOffset_.y });
-}
