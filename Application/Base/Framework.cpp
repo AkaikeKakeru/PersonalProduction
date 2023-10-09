42e73f7e211c33a39453f2890ac93a1e301a942b
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-10-09 23:19:30 +0900

    白塗り画像のロード

--- Application/Base/Framework.cpp
+++ Application/Base/Framework.cpp
@@ -61,6 +61,7 @@ void Framework::Initialize(){
 	spriteBas->LoadTexture(1, "cursor.png");
 	spriteBas->LoadTexture(2, "HPgauge.png");
 
+	spriteBas->LoadTexture(kWhiteTextureIndex_, "white1x1.png");
 	spriteBas->LoadTexture(kTextTextureIndex_, "debugfont.png");
 
 	//パーティクルマネージャー
