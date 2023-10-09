c5f2c3a76f3a3324a8ba207944bc520243b306d7
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-10-09 23:37:17 +0900

    画像ファイル名の誤字修正

--- Application/Base/Framework.cpp
+++ Application/Base/Framework.cpp
@@ -61,7 +61,7 @@ void Framework::Initialize(){
 	spriteBas->LoadTexture(1, "cursor.png");
 	spriteBas->LoadTexture(2, "HPgauge.png");
 
-	spriteBas->LoadTexture(kWhiteTextureIndex_, "white1x1.png");
+	spriteBas->LoadTexture(kWhiteTextureIndex_, "white.png");
 	spriteBas->LoadTexture(kTextTextureIndex_, "debugfont.png");
 
 	//パーティクルマネージャー
