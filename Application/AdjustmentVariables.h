377381e0c718b4e38f8befdbd7d790e8d90d2f2e
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-11-08 15:57:42 +0900

    データ、グループ、項目、値の階層を用意

--- Application/AdjustmentVariables.h
+++ Application/AdjustmentVariables.h
@@ -1,8 +1,28 @@
 ﻿/*調整項目をひとまとめにしたい*/
 #pragma once
+#include <Vector3.h>
+#include <variant>
+#include <map>
 
-//グローバル変数
+//調整項目変数のまとめ
 class AdjustmentVariables {
+public: //構造
+	//項目 の構造
+	struct Item {
+		//値
+		std::variant<int32_t, float, Vector3> value_;
+	};
+
+	//グループの構造
+	struct Group {
+		//項目
+		std::map<std::strong_ordering, Item> items_;
+	};
+
+private: //変数
+	//全データ
+	std::map<std::strong_ordering, Group> datas_;
+
 private: //シングルトン化
 	AdjustmentVariables() = default;
 	~AdjustmentVariables() = default;
