ca64d1f546712678aa8668e604829b7d8a4a7daf
agane <k021g1137@g.neec.ac.jp>
2024-06-28 13:35:17 +0900

    ステージフィールド作業途中

--- Application/Game/Background/StageField.cpp
+++ Application/Game/Background/StageField.cpp
@@ -14,24 +14,102 @@
 void StageField::Initialize() {
     levelLoader_= new LevelLoader();
     levelLoader_->LoadFileJson(jsonFileName_, camera_);
+
+	for(auto& loadObj : levelLoader_->GetObjects()) {
+		//Vector3 pos = loadObj->GetPosition();
+
+		//loadObj->SetPosition({ pos.x,pos.y,kTubePopPosZ_ });
+
+		Object3d* newTube = Object3d::Create();
+		newTube->Initialize();
+
+		newTube->SetScale(loadObj->GetScale());
+		newTube->SetRotation(loadObj->GetRotation());
+		newTube->SetPosition(loadObj->GetPosition());
+
+		newTube->SetModel(loadObj->GetModel());
+
+		newTube->SetCamera(camera_);
+		newTube->Update();
+
+		defaultObjects_.push_back(newTube);
+	}
 }
 
 void StageField::Update() {
-    for(auto& block : levelLoader_->GetObjects()) {
-        block->Update();
-    }
+
+	//Vector3 speed{ 0,0,-16.0f };
+
+	//timerRePop_++;
+
+	//if (!isStop_) {
+
+	//	if (timerRePop_ >= kRePopTime_) {
+	//		AddTube();
+
+	//		timerRePop_ = 0;
+	//	}
+	//}
+
+	for (Object3d*& tube : objects_) {
+		//Vector3 pos = tube->GetPosition();
+
+		//pos += speed;
+
+		//tube->SetPosition(pos);
+
+		tube->Update();
+
+		//if (tube->GetPosition().z <= kTubeDeadPosZ_) {
+		//	objects_.remove(tube);
+		//}
+	}
+
+	//objects_.remove_if([](Object3d*& obj) {
+	//	return obj->GetPosition().z <= -300.0f;
+	//	});
+
+	for (auto& tube : defaultObjects_) {
+		tube->Update();
+	}
 }
 
 void StageField::Draw() {
-    for(auto& block : levelLoader_->GetObjects()) {
-        block->Draw();
-    }
+	//for (auto& tube : objects_) {
+	//	tube->Draw();
+	//}
+	for (auto& tube : defaultObjects_) {
+		tube->Draw();
+	}
 }
 
 void StageField::Finalize() {
-    for (Object3d*& block : levelLoader_->GetObjects()) {
-        delete block;
-    }
-
+	objects_.clear();
+	defaultObjects_.clear();
     levelLoader_->Finalize();
+}
+
+void StageField::AddTube(/*
+	const Vector3 pos,
+	const Vector3 rota,
+	const Vector3 scale*/) {
+	for(auto& loadObj : levelLoader_->GetObjects()) {
+		//Vector3 pos = loadObj->GetPosition();
+
+		//loadObj->SetPosition({ pos.x,pos.y,kTubePopPosZ_ });
+
+		Object3d* newTube = Object3d::Create();
+		newTube->Initialize();
+
+		newTube->SetScale(loadObj->GetScale());
+		newTube->SetRotation(loadObj->GetRotation());
+		newTube->SetPosition(loadObj->GetPosition());
+
+		newTube->SetModel(loadObj->GetModel());
+
+		newTube->SetCamera(camera_);
+		newTube->Update();
+
+		objects_.push_back(newTube);
+	}
 }
--- Application/Game/Background/StageField.h
+++ Application/Game/Background/StageField.h
@@ -15,14 +15,20 @@
 struct LevelData;
 
 class StageField {
-public:
-    static const int cBlockCount_ = 100;
-
+public: //クラス内定数
+    const float kTubePopPosZ_ = 600.0f;
+    const float kTubeDeadPosZ_ = -300.0f;
+    const int32_t kRePopTime_ = 10;
 public:
     void Initialize();
     void Update();
     void Draw();
     void Finalize();
+
+    void AddTube(/*
+        const Vector3 pos,
+        const Vector3 rota,
+        const Vector3 scale*/);
 
 public:
     void SetCamera(Camera* camera) {
@@ -40,12 +46,24 @@ public:
 private:
     Camera* camera_;
 
+    //デフォルトオブジェクト
+    std::list<Object3d*> defaultObjects_;
+
     //オブジェクトデータコンテナ
-    std::vector<Object3d*> objects_;
+    std::list<Object3d*> objects_;
 
     //レベルローダー
     LevelLoader* levelLoader_;
 
     //読み込むjson名
     std::string jsonFileName_;
+
+    //スピード
+    //float speed_ = 0.0f;
+
+    //再配置タイマー
+    int32_t timerRePop_ = 0;
+
+    //停止フラグ
+    bool isStop_ = false;
 };
